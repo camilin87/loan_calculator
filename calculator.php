7d07e1a27e87fb7751984da4d95f40d5ca3acbe3
Alexandra Saldana <alexsaldana9@gmail.com>
2018-04-25 19:31:12 -0400

    results show, if there is a pay off date

--- calculator.php
+++ calculator.php
@@ -9,18 +9,37 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 
     <style>
-    body {
-    margin-top: 100px;
-    margin-bottom: 100px;
-    margin-right: 150px;
-    margin-left: 80px;
-    background-color: lightblue;
-    }
+      header {
+        background-color: #3B5CB2;
+        width:100%;
+        text-align: center;
+        border-radius: 10px;
+        top:0;
+        left:0;
+      }
 
-    h1 {
-      text-align: center;
-    }
+     .btn-primary {
+        background-color: #3B5CB2;
+        border-color: #3B5CB2;
+        width: 100%;
+      }
 
+      body {
+        margin-top: 100px;
+        margin-bottom: 100px;
+        margin-right: 150px;
+        margin-left: 80px;
+        background-color: lightblue;
+      }
+
+      h1 {
+        text-align: center;
+        color: white;
+      }
+
+      #results {
+        padding-left: 50px;
+      }
     </style>
   </head>
 
@@ -29,9 +48,9 @@
 
       $start_date = 0;
       $loan_amount = 0;
-      $installment_amount = 0;
+      $installment_amount = 0.01;
       $interest_rate = 0;
-      $installment_interval = 0;
+      $installment_interval = 1;
       $estimate_payoff = 0;
 
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -84,12 +103,12 @@
 
       $start_date_new_format = strtotime($start_date);
 
-      echo "DEBUG: start date new format: " . date('Y/m/d', $start_date_new_format) . '<br>';
-      echo "DEBUG: installment_interval " . $installment_interval . '<br>';
-      // echo "DEBUG: start plus 7 days " . date('Y/m/d', strtotime("+7 day", $start_date_new_format)) . '<br>';
-      // echo "DEBUG: start plus 3 month " . date('Y/m/d', strtotime("+3 month", $start_date_new_format)) . '<br>';
-      // echo "DEBUG: start plus 5 weeks " . date('Y/m/d', strtotime("+5 week", $start_date_new_format)) . '<br>';
-      echo "DEBUG: number of periods rounded: " . $number_of_periods_rounded . '<br>';
+      //echo "DEBUG: start date new format: " . date('Y/m/d', $start_date_new_format) . '<br>';
+      //echo "DEBUG: installment_interval " . $installment_interval . '<br>';
+      //echo "DEBUG: start plus 7 days " . date('Y/m/d', strtotime("+7 day", $start_date_new_format)) . '<br>';
+      //echo "DEBUG: start plus 3 month " . date('Y/m/d', strtotime("+3 month", $start_date_new_format)) . '<br>';
+      //echo "DEBUG: start plus 5 weeks " . date('Y/m/d', strtotime("+5 week", $start_date_new_format)) . '<br>';
+      //echo "DEBUG: number of periods rounded: " . $number_of_periods_rounded . '<br>';
     
       $payments = array();
 
@@ -97,7 +116,7 @@
         $pay_off_date = strtotime("+".$number_of_periods_rounded ." day", $start_date_new_format); 
 
         for ($payment_date = $start_date_new_format; $payment_date <= $pay_off_date; $payment_date = strtotime("+1 day", $payment_date)){
-          echo "DEBUG: Payment date (daily): " . date('Y/m/d', $payment_date) . '<br>';
+          //echo "DEBUG: Payment date (daily): " . date('Y/m/d', $payment_date) . '<br>';
 
           array_push($payments, $payment_date);
         } 
@@ -108,7 +127,7 @@
         $pay_off_date = strtotime("+".$number_of_periods_rounded ." week", $start_date_new_format);
 
         for ($payment_date = $start_date_new_format; $payment_date <= $pay_off_date; $payment_date = strtotime("+1 week", $payment_date)){
-          echo "DEBUG: Payment date (weekly): " . date('Y/m/d', $payment_date) . '<br>';
+          //echo "DEBUG: Payment date (weekly): " . date('Y/m/d', $payment_date) . '<br>';
 
           array_push($payments, $payment_date);
         } 
@@ -118,95 +137,81 @@
         $pay_off_date = strtotime("+".$number_of_periods_rounded ." month", $start_date_new_format);  
 
         for ($payment_date = $start_date_new_format; $payment_date <= $pay_off_date; $payment_date = strtotime("+1 month", $payment_date)){
-          echo "DEBUG: Payment date (monthly): " . date('Y/m/d', $payment_date) . '<br>';
+          //echo "DEBUG: Payment date (monthly): " . date('Y/m/d', $payment_date) . '<br>';
 
           array_push($payments, $payment_date);
         }  
       }
 
-      echo "DEBUG Pay off date formatted: " . date('Y-m-d',$pay_off_date) . "<br>";
+      //echo "DEBUG Pay off date formatted: " . date('Y-m-d',$pay_off_date) . "<br>";
     ?>
 
-    <h1>Loan repayment calculator in PHP</h1>
+    <header>
+      <div class="container text-center">
+        <h1>Loan Repayment Calculator in PHP </h1>
+      </div>
+    </header>
+    <br>
 
     <form method = "post" action = "/calculator.php">
+      <div class="form-row">
+        <div class="form-group col-md-3">
+          <div class="form-group">
+            <label for="start_date">Start Date:</label>
+            <input type="date" class="form-control" id="start_date" name="start_date"  value="<?php echo isset($_POST["start_date"]) ? $_POST["start_date"] : ''; ?>">
+          </div>
 
-      <div class="form-group">
-        <label for="start_date">Start Date:</label>
-        <input type="date" class="form-control" id="start_date" name="start_date"  value="<?php echo isset($_POST["start_date"]) ? $_POST["start_date"] : ''; ?>">
-      </div>
+          <div class="form-group">
+              <label for="loan_amount">Loan Amount:</label>
+              <input type="text" class="form-control" id="loan_amount" name="loan_amount" placeholder="$" value="<?php echo isset($_POST["loan_amount"]) ? $_POST["loan_amount"] : ''; ?>">
+          </div>
 
-      <div class="form-group">
-          <label for="loan_amount">Loan Amount:</label>
-          <input type="text" class="form-control" id="loan_amount" name="loan_amount" placeholder="$" value="<?php echo isset($_POST["loan_amount"]) ? $_POST["loan_amount"] : ''; ?>">
-      </div>
+          <div class="form-group">
+              <label for="installment_amount">Installment Amount:</label>
+              <input type="text" class="form-control" id="installment_amount" name="installment_amount" placeholder="$" value="<?php echo isset($_POST["installment_amount"]) ? $_POST["installment_amount"] : ''; ?>">
+          </div>
 
-      <div class="form-group">
-          <label for="installment_amount">Installment Amount:</label>
-          <input type="text" class="form-control" id="installment_amount" name="installment_amount" placeholder="$" value="<?php echo isset($_POST["installment_amount"]) ? $_POST["installment_amount"] : ''; ?>">
-      </div>
+          <div class="form-group">
+              <label for="interest_rate">Interest Rate:</label>
+              <input type="text" class="form-control" id="interest_rate" name="interest_rate" placeholder="%" value="<?php echo isset($_POST["interest_rate"]) ? $_POST["interest_rate"] : ''; ?>">
+          </div>
 
-      <div class="form-group">
-          <label for="interest_rate">Interest Rate:</label>
-          <input type="text" class="form-control" id="interest_rate" name="interest_rate" placeholder="%" value="<?php echo isset($_POST["interest_rate"]) ? $_POST["interest_rate"] : ''; ?>">
-      </div>
+          <div class="form-group">
+            <label for="installment_interval">Installment Interval:</label>
+            <div class="btn-group" data-toggle="buttons">
+              <label class="btn">
+                <input type="radio" name="installment_interval" id="daily" value="365"> Daily
+              </label>
+              <label class="btn">
+                <input type="radio" name="installment_interval" id="weekly" value="52"> Weekly
+              </label>
+              <label class="btn">
+                <input type="radio" name="installment_interval" id="monthly" value="12"> Monthly
+              </label>
+            </div>
+          </div>
 
-      <div class="form-group">
-        <label for="installment_interval">Installment Interval:</label>
-        <div class="btn-group" data-toggle="buttons">
-          <label class="btn">
-            <input type="radio" name="installment_interval" id="daily" value="365"> Daily
-          </label>
-          <label class="btn">
-            <input type="radio" name="installment_interval" id="weekly" value="52"> Weekly
-          </label>
-          <label class="btn">
-            <input type="radio" name="installment_interval" id="monthly" value="12"> Monthly
-          </label>
-        </div>
-      </div>
-
-      <br> 
-      
-      <input type="submit" value="Calculate" class="btn btn-primary">
-
+          <br> 
+          
+          <input type="submit" value="Calculate" class="btn btn-primary">
+        </div>      
     </form>
+   
+    <?php if (isset($pay_off_date)){ ?>
+      <div class="form-group col-md-6" id="results">
+        <h2>Payoff Date: <?php echo  date('Y/m/d', $pay_off_date); ?></h2>
 
-
-    <div class="result">
-
-      <!-- show calculations -->
-      <h2>Payoff Date: <?php echo  date('Y/m/d', $pay_off_date); ?></h2>
-
-      <h2>Payment Dates: </h2>
-      <ul>
-      <?php foreach ($payments as $payment) { ?>
-        <li><?php echo date('Y/m/d', $payment) . '<br>';?></li>
-      <?php } ?>
-      </ul>
+        <h2>Payment Dates: </h2>
+          <ul>
+          <?php foreach ($payments as $payment) { ?>
+            <li><?php echo date('Y/m/d', $payment) . '<br>';?></li>
+          <?php } ?>
+          </ul>
+      </div>
+    <?php } ?>
     </div>
 
+     
 
-    <?php
-      echo "<h2>You selected :</h2>";
-      echo "<br>";
-
-      echo "Start date: " . $start_date;
-      echo "<br>";
-
-      echo "Loan amount: " . $loan_amount;
-      echo "<br>";
-
-      echo "Installment amount: " . $installment_amount;
-      echo "<br>";
-
-      echo "Interest rate: " . $interest_rate;
-      echo "<br>";
-
-      echo "Installment interval: " . $installment_interval;
-      echo "<br>";
-
-      echo "Output of periods function: " . $number_of_periods;
-    ?>  
   </body>
 </html>
